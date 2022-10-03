8c64f9dd57c7760fbdc15c3dfa25ce884d4db8fe
juanxxy <104758712+juanxxy@users.noreply.github.com>
2022-10-04 00:37:22 +0800

    personal page

---
+++ PersonalPage/0813248_hw2.js
@@ -0,0 +1,17 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+// ..
+AOS.init();
+
+AOS.init({
+  duration: 2000,
+  easing: 'ease-in-out',
+  once: true,
+  mirror: false
+})
+ 
+    
+
+    
+    
+  
